612790e4cdf70f7236dd560885f82b7f18df8cdd
auravmaurya <auravmaurya@gmail.com>
2015-02-12 18:57:24 +0530

    12feb
    1

---
+++ welcome.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+	<title> Hello World</title>
+</head>
+<body>
+
+Welcome,
+	<?php echo $_POST["name"]; ?><br>
+	Your Email id is <?php echo $_POST["email"];?> <br> 
+
+</body>
+</html>
